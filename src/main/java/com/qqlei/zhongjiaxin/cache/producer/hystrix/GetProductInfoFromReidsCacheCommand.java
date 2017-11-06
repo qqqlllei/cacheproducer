c29437f8e6007ff2966c6bbb332860d656ed8764
lilei <861971291@qq.com>
2017-11-06 15:19:31 +0800

    对redis的降级策略参数配置

--- src/main/java/com/qqlei/zhongjiaxin/cache/producer/hystrix/GetProductInfoFromReidsCacheCommand.java
+++ src/main/java/com/qqlei/zhongjiaxin/cache/producer/hystrix/GetProductInfoFromReidsCacheCommand.java
@@ -3,6 +3,7 @@ package com.qqlei.zhongjiaxin.cache.producer.hystrix;
 import com.alibaba.fastjson.JSONObject;
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandProperties;
 import com.qqlei.zhongjiaxin.cache.producer.model.ProductInfo;
 import com.qqlei.zhongjiaxin.cache.producer.spring.SpringContext;
 import redis.clients.jedis.JedisCluster;
@@ -14,7 +15,17 @@ public class GetProductInfoFromReidsCacheCommand extends HystrixCommand<ProductI
 
     private Long productId;
     public GetProductInfoFromReidsCacheCommand(Long productId){
-        super(HystrixCommandGroupKey.Factory.asKey("RedisGroup"));
+        super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("RedisGroup"))
+                .andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+                        //timeout超时控制,此处的100ms是根据对于redis的请求的一个统计得出的，例如100个请求，99个都是在100ms内完成
+                        .withExecutionTimeoutInMilliseconds(100)
+                        //如果设置为20（默认值），那么在一个10秒的滑动窗口内，如果只有19个请求，即使这19个请求都是异常的，也是不会触发开启短路器的
+                        .withCircuitBreakerRequestVolumeThreshold(1000)
+                        //当异常请求达到这个百分比时，就触发打开短路器，默认是50，也就是50%
+                        .withCircuitBreakerErrorThresholdPercentage(70)
+                        //需要在多长时间内直接reject请求，然后在这段时间之后，再重新导holf-open状态，尝试允许请求通过以及自动恢复，默认值是5000毫秒
+                        .withCircuitBreakerSleepWindowInMilliseconds(5000))
+        );
         this.productId = productId;
     }
 
